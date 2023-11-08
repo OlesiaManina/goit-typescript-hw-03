22e956aeb24a444f951cb49baecd6e98f9e9d28f
OlesiaManina <113991075+OlesiaManina@users.noreply.github.com>
2023-11-08 17:09:22 +0200

    added access modifiers

--- src/4.ts
+++ src/4.ts
@@ -16,8 +16,8 @@ class Person {
 }
 
 abstract class House {
-    door: boolean = false;
-    tenants: Person[] = [];
+    protected door: boolean = false;
+    protected tenants: Person[] = [];
 
     constructor(key: Key) {
     }
